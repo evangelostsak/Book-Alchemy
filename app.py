61852f66804dacad70bbe70fbc8b33be0fc5cb0b
evangelostsak <baggtsak55@gmail.com>
2024-12-06 00:16:28 +0100

    path bug fix, db init

--- app.py
+++ app.py
@@ -1,2 +1,16 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from data_models import db, Author, Book
+import os
+
+# Init flask instance
+app = Flask(__name__)
+
+# data path bug fix
+base_dir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{os.path.join(base_dir, "data", "library.sqlite3")}'
+
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
