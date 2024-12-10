7136223ae8a0c0dad7ee6bf537cbd8bf36b91b6f
evangelostsak <baggtsak55@gmail.com>
2024-12-10 02:49:53 +0100

    updated models to include rating

--- data_models.py
+++ data_models.py
@@ -22,6 +22,7 @@ class Author(db.Model):
     name = Column(String, nullable=False)
     birth_date = Column(Date, nullable=True)
     death_date = Column(Date, nullable=True)
+    book = relationship('Book', backref='author', cascade="all, delete", lazy=True)
 
     def __repr__(self):
         """
@@ -53,15 +54,14 @@ class Book(db.Model):
     isbn = Column(Integer, nullable=False, unique=True)
     title = Column(String, nullable=False)
     publication_year = Column(Integer, nullable=True)
+    rating = Column(Integer, nullable=True, default=0)
     cover_img_url = Column(String, nullable=True)
-
-    author = relationship('Author', backref='books', lazy=True)
 
     def __repr__(self):
         """Returns a string representation of the Book model, debugging-friendly"""
         return (f"Book(id = {self.id}, isbn = {self.isbn}, title = {self.title}, "
                 f"publication_year = {self.publication_year}, cover_url = {self.cover_url}, "
-                f"description = {self.description})")
+                f"rating = {self.rating})")
 
     def __str__(self):
         """Returns a human-readable string of the Book model"""
